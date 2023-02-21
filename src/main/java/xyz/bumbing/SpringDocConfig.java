b819e34f4913c543d998e7581aff702284fa85f5
Beomhee Han <kjhhjk1234@gmail.com>
2023-02-21 14:47:49 +0900

    init

---
+++ src/main/java/xyz/bumbing/SpringDocConfig.java
@@ -0,0 +1,10 @@
+package xyz.bumbing;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+public class SpringDocConfig {
+}
